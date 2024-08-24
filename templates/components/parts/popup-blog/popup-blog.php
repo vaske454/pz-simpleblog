70e98d9c839b67e67213c28a6d48a8d9f2dafc7f
vaske454 <vasilijetomovic98@gmail.com>
2024-08-24 10:12:29 +0200

    If the user is logged in, add a statement, then show the create and delete buttons.

--- templates/components/parts/popup-blog/popup-blog.php
+++ templates/components/parts/popup-blog/popup-blog.php
@@ -27,16 +27,20 @@
                             <?php endforeach; ?>
                         </select>
                     <?php endif; ?>
+                    <?php if (isset($_SESSION['user'])): ?>
                     <input type="hidden" id="popup-blog-id" name="id" />
                     <button type="submit" id="save-blog-button" class="save-button" name="action" value="update">Save Changes</button>
+                    <?php endif; ?>
                 </footer>
             </article>
         </form>
 
+        <?php if (isset($_SESSION['user'])): ?>
         <!-- Delete Form -->
         <form action="/delete-blog" method="POST" id="delete-blog-form">
             <input type="hidden" id="delete-blog-id" name="id" />
             <button type="submit" id="delete-blog-button" class="delete-button" name="action" value="delete">Delete</button>
         </form>
+        <?php endif; ?>
     </div>
 </div>
